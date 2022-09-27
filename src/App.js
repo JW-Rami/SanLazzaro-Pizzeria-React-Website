44bfe84fdd2b0b0e9d59baf6124e2f36b153a6d5
Rami <rami@jwcorp.fr>
2022-09-27 23:45:30 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
+// import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
 import Foods from "./components/Foods/Foods";
 import Footer from "./components/Footer/Footer";
 import Loading from "./components/Loading/Loading";
+import TableReservation from "./components/TableReservation/TableReservation";
+import { InlineWidget } from "react-calendly";
+
 
 function App() {
     const [isLoading, setIsLoading] = useState(true);
@@ -17,15 +21,17 @@ function App() {
     }
 
     return (
+
         <div className="app">
+            {/* <Routes>
+            <Route path="/TableReservation"> element={<TableReservation /> }</Route>
+            </Routes> */}
+            
             <Header />
             <Hero />
-            
             <Foods />
+            <TableReservation/>
             <Footer />
-            
-           
-
         </div>
     );
 }
--- src/components/TableReservation/TableReservation.js
+++ src/components/TableReservation/TableReservation.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+// import { Routes, Route } from "react-router-dom";
+import { InlineWidget } from "react-calendly";
 
 function TableReservation() {
-    return(
-        <section>
-            
-        </section>
-    )
+return (
+    <div id="TableReservation">
+
+    <InlineWidget url="https://calendly.com/arthur-bineau/sanlazzaro?background_color=ddf2cd&primary_color=ddf2cd" />
+
+    </div>
+)
 }
 
 export default TableReservation;
