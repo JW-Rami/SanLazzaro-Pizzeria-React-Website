6311e8751013ca15bb185c4e1c5dbc4df9f2f839
Rami <rami@jwcorp.fr>
2022-09-29 06:19:35 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
+
 // import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
@@ -9,6 +12,7 @@ import Footer from "./components/Footer/Footer";
 import Loading from "./components/Loading/Loading";
 import TableReservation from "./components/TableReservation/TableReservation";
 import { InlineWidget } from "react-calendly";
+import {BrowserRouter, Routes} from 'react-router-dom';
 
 
 function App() {
@@ -26,7 +30,13 @@ function App() {
             {/* <Routes>
             <Route path="/TableReservation"> element={<TableReservation /> }</Route>
             </Routes> */}
-            
+            <BrowserRouter >
+               <Routes>
+                {/* <Route path="/tablereservation" element={<h1>Bonsoir</h1>}/> */}
+                
+               </Routes>
+            </BrowserRouter>
+
             <Header />
             <Hero />
             <Foods />
