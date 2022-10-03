65c3c13859466849f8edfaec52001dcdb18f1cf7
Rami <rami@jwcorp.fr>
2022-10-03 02:42:09 +0200

    .

--- src/components/italianfoods/ItalianFoods.js
+++ src/components/italianfoods/ItalianFoods.js
@@ -1,13 +1,15 @@
 // import pizzaspinash from "../../assets/images/"
-import "./ItalianFoods.css"
+import "./ItalianFoods.css";
 
 function ItalianFoods() {
-    return (
-   <section className="ItalianFoods">
-        <div className="">
-            
-        </div>        
-   </section> 
-   )
+  return (
+    <section className="ItalianFoods">
+      <div className="starter">Nos entrées</div>
+      <div className="pizzas">Nos pizzas</div>
+      <div className="delicatessen">Charcuterie italienne</div>
+      <div className="seafoodspeciality">Nos spécialités de la mer</div>
+      <div className="vins">Nos vins</div>
+    </section>
+  );
 }
 export default ItalianFoods;
